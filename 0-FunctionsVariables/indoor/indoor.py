adf07ab8cb482e422ec60846a5c0c734a04b096a
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-05 11:33:22 +0000

    Upload compleeted 0-1 funcvar and prob01

---
+++ 0-FunctionsVariables/indoor/indoor.py
@@ -0,0 +1,7 @@
+name = input("try and scream. u prob sound weak!").strip().lower()
+
+# both remove the whitespace and decapitalise users input
+
+
+print(f"YOU THINK YOU CAN OUT SCREAM ME!?,you sound like this!")
+print(f"{name}")
