9e09d24c0748a8c9e7c40640110e32a4e30b8a7c
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-06 22:39:39 +0000

    faces incomplete

--- 0-FunctionsVariables/indoor/indoor.py
+++ 0-FunctionsVariables/indoor/indoor.py
@@ -3,5 +3,5 @@ name = input("try and scream. u prob sound weak!").strip().lower()
 # both remove the whitespace and decapitalise users input
 
 
-print(f"YOU THINK YOU CAN OUT SCREAM ME!?,you sound like this!")
+print("YOU THINK YOU CAN OUT SCREAM ME!?,you sound like this!")
 print(f"{name}")
