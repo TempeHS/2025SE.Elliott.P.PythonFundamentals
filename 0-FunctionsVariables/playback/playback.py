373779a0ee607b0016ee097b91bca26879295f67
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-06 12:03:05 +0000

    playback

--- 0-FunctionsVariables/playback/playback.py
+++ 0-FunctionsVariables/playback/playback.py
@@ -1,1 +1,11 @@
-prompt = input("what did i just say?")
+# defome input string as returning "replace" with every " " as a "..."
+def playback(input_str):
+    return input_str.replace(" ", "...")
+
+
+# asking user for sentence
+user = input("Enter a sentence: ")
+# reading conversion
+result = playback(user)
+# printing converted version
+print(result)
