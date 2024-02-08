6b1bc89409792981357d6dc4b2af273fb9a73d00
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-08 02:00:40 +0000

    test

--- 1-Conditionals/deep/deep.py
+++ 1-Conditionals/deep/deep.py
@@ -1,4 +1,4 @@
-poopFingleBottom = input(f"what is the answer to the universe")
+poopFingleBottom = input("what is the answer to the universe")
 poopFingleBottom = poopFingleBottom.strip(" ").lower()
 if "42" or "forty two" or "forty-two" or "fortytwo" in poopFingleBottom:
     print("Yes.")
