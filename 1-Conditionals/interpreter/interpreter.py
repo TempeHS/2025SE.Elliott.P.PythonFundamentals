0f205dece27d1bbc55284b2f30c6021b94cb8b8d
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-12 01:24:31 +0000

    first idea

--- 1-Conditionals/interpreter/interpreter.py
+++ 1-Conditionals/interpreter/interpreter.py
@@ -1,7 +1,21 @@
-
-x = int(input("what is your first number?"))
+x = float(input("what is your first number?"))
 y = input("what is your operator")
+z = float(input("what is your second number!?"))
 
-def changer :
-    if y == "+" :
-    print("+")
+x = round(x, 1)
+z = round(z, 1)
+
+
+def changer():
+
+    if y == "+":
+        print(x + z)
+    elif y == "-":
+        print(x - z)
+    elif y == "/" or y == "%":
+        print(x / z)
+    elif y == "*" or y == "x":
+        print(x * z)
+
+
+changer()
