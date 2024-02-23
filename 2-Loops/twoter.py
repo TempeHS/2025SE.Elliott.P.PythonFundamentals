97a9d50dfa17bf815873be33a093dc88706d9daa
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-23 03:08:39 +0000

    update

--- 2-Loops/twoter.py
+++ 2-Loops/twoter.py
@@ -2,10 +2,11 @@
 
 
 def remove_vowels(text):
-    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    vowels = ["a", "e", "i", "o", "u"]
     result = "".join([char for char in text if char not in vowels])
     return result
 
 
 user_input = input("Please enter your text: ")
+user_input = user_input.lower()
 print(remove_vowels(user_input))
