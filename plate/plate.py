6f92fde0e66c61a90c400df7445a6e38d63fb1aa
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-02-25 22:54:44 +0000

    plate completed

--- plate/plate.py
+++ plate/plate.py
@@ -1,20 +1,5 @@
-print("All vanity plates must start with at least two letters.")
-print(
-    "… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters."
-)
-print(
-    "Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a 0."
-)
-print("No periods, spaces, or punctuation marks are allowed.")
-# ^^^ requirements
-
-plate = input("Plate: ")
-badWords = [",", ".", "!", " ", "?"]
-badCharecters ["1","2","3","4","5","6",]
-
-
 def main():
-
+    plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
     else:
@@ -22,10 +7,26 @@ def main():
 
 
 def is_valid(s):
-    if plate in badWords:
+    if len(s) < 2 or len(s) > 6:
         return False
-    elif plate.startswith()
+    if s[0].isalpha() == False or s[1].isalpha() == False:
+        return False
+
+    i = 0
+    while i < len(s):
+        if s[i].isalpha() == False:
+            if s[i] == "0":
+                return False
+            elif not s[i:].isdigit():
+                return False
+            else:
+                break
+        i += 1
+    for c in s:
+        if c in [".", " ", "!", "?", ","]:
+            return False
+    else:
+        return True
 
 
 main()
-
