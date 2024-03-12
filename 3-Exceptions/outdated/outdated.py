41c1dbf34634d23d0ae696638a7ef4d8a27bfda0
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-03-12 05:20:39 +0000

    exceptions complete

--- 3-Exceptions/outdated/outdated.py
+++ 3-Exceptions/outdated/outdated.py
@@ -17,15 +17,15 @@ def outdated():
         date_input = input(
             "Enter a date in MM/DD/YYYY or Month DD, YYYY format: "
         ).strip()
-        if "/" in date_input:
+        if "/" in date_input:  # if 1/2/3333
             month, day, year = date_input.split("/")
             if validate_date(month, day, year):
                 print(year.zfill(4) + "-" + month.zfill(2) + "-" + day.zfill(2))
                 return
-        elif "," in date_input:
-            month, day_year = date_input.split(" ", 1)
-            day, year = day_year.replace(",", "").split()
-            month = month_mapping[month]
+        elif "," in date_input:  # if January the second, 3333
+            month, day_year = date_input.split(" ", 1)  # [January] { } [second]
+            day, year = day_year.replace(",", "").split()  # [second] {,} [3333]
+            month = month_mapping[month]  # month_mapping[January] >>>1
             if validate_date(month, day, year):
                 print(year.zfill(4) + "-" + month.zfill(2) + "-" + day.zfill(2))
                 return
@@ -34,6 +34,8 @@ def outdated():
 
 def validate_date(month, day, year):
     return 1 <= int(month) <= 12 and 1 <= int(day) <= 31
+    # return if value is <= 12 or value is <=31
+    # if it isnt it will return false
 
 
 if __name__ == "__main__":
