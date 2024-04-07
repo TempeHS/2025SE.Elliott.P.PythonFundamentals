34c81f63e78ed7c94c5d230487579caf441dada7
elliottp123 <124545912+elliottp123@users.noreply.github.com>
2024-04-07 01:24:36 +0000

    scourgify completed

---
+++ 6-FileIO/scourgify/scourgify.py
@@ -0,0 +1,34 @@
+import csv
+import sys
+
+# Verify the correct number of command-line arguments
+if len(sys.argv) != 3:
+    print("Usage: python scourgify.py input.csv output.csv")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# Read the input CSV file
+try:
+    with open(input_file, "r") as csv_file:
+        students = list(csv.reader(csv_file))
+        students.pop(0)  # remove the header
+except IOError:
+    print("Unable to open", input_file)
+    sys.exit(1)
+
+# Write to the output CSV file
+try:
+    with open(output_file, "w") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["last", "first", "house"])
+        for student in students:
+            name, house = student
+            first_name, last_name = name.split(", ")
+            writer.writerow([last_name, first_name, house])
+except IOError:
+    print("Failed to write to", output_file)
+    sys.exit(1)
+
+print("Successfully formatted", input_file, "and wrote to", output_file)
